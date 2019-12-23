70438cde11d9a7af7ce337923d18349d01d408da
Innoox <chypakabra@gmail.com>
2019-12-23 20:26:26 +0100

    6_3

--- Test/src/exceptions/Task_6_1.java
+++ Test/src/exceptions/Task_6_1.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Task_6_1 {
 
-    void myMethod() {
+    void myMethod() throws IOException {
         System.out.println("Some error.");
     }
 
--- Test/src/exceptions/task_6_2/Main.java
+++ Test/src/exceptions/task_6_2/Main.java
@@ -1,4 +1,21 @@
 package exceptions.task_6_2;
 
+import java.io.IOException;
+
 public class Main {
+
+    static void myMethod(char[] tmp){
+        int tmpL = tmp.length + 1;
+        System.out.println(tmp[tmpL]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        char[] myArr = {'H', 'l', 'l', 'o'};
+        try {
+            myMethod(myArr);
+            throw new IOException();
+        } catch (IOException ex){
+            System.out.println(ex);
+        }
+    }
 }
