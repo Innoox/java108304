74a4c1f7c03e09e745fcef25b0c56d3da61499d5
Innoox <chypakabra@gmail.com>
2019-12-23 20:10:39 +0100

    task 6 1
    new one

---
+++ Test/src/exceptions/Task_6_1.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+import java.io.IOException;
+
+public class Task_6_1 {
+
+    void myMethod() {
+        System.out.println("Some error.");
+    }
+
+}
+
+class SubClass extends Task_6_1 {
+    void myMethod() throws IOException
+    {
+        System.out.println("Some error.");
+    }
+
+    public static void main(String args[]){
+        Task_6_1 myObj = new Task_6_1();
+        try {
+            myObj.myMethod();
+        } catch (IOException ex){
+            System.out.println(ex);
+        }
+    }
+}
